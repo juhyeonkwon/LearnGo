544631ffb529b58955cb41934c210ff4eeec2c21
kwonjuhyeon <asshole96@naver.com>
2020-03-02 20:39:38 +0900

    learnGo

---
+++ gorm/gorm_connection.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //You could import dialect
+)
+
+type User struct {
+	ID int
+	Username string
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "golang:wngus123@tcp(127.0.0.1:3306)/golang?charset=utf8&parseTime=True&loc=Local")
+
+	defer db.Close()
+
+	if err != nil {
+		log.Println("Connection Failed")
+	} else {
+		log.Println("Connected!")
+	}
+}
+*/
---
+++ net.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Printf("served: %s\n", r.Host)
+}
+
+func main() {
+	PORT := ":3333"
+
+	http.HandleFunc("/", mainHandler)
+
+	err := http.ListenAndServe(PORT, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+*/
